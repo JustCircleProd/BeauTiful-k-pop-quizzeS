52b8a1129dfa9c1a38439a8261c86ea1aaafa17f
VadimKarch <vadimkarworkmail@gmail.com>
2021-06-08 12:55:20 +0300

    Initial setup, binding, creating a theme, building two layouts

---
+++ app/src/main/java/ru/mytest/onlybtsfuns/MainActivity.kt
@@ -0,0 +1,22 @@
+package ru.mytest.onlybtsfuns
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.LayoutInflater
+import ru.mytest.onlybtsfuns.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.randomQuestions.setOnClickListener {
+            val intent = Intent(this, QuizActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
