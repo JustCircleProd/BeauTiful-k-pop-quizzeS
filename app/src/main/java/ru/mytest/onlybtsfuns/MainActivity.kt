5facafdc1b5226f76288254dc487ef789733a22e
VadimKarch <vadimkarworkmail@gmail.com>
2021-06-16 22:21:30 +0300

    Scores now pass with intent

--- app/src/main/java/ru/mytest/onlybtsfuns/MainActivity.kt
+++ app/src/main/java/ru/mytest/onlybtsfuns/MainActivity.kt
@@ -3,6 +3,7 @@ package ru.mytest.onlybtsfuns
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.room.Room
 import ru.mytest.onlybtsfuns.data.AppDatabase
 import ru.mytest.onlybtsfuns.data.Score
@@ -11,22 +12,23 @@ import ru.mytest.onlybtsfuns.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private var countOfQuestions = 6
+    private lateinit var db: AppDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "db.db")
+        db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "db.db")
             .createFromAsset("database/db.db").allowMainThreadQueries().build()
 
-        binding.randomQuestions.setOnClickListener { showRandomQuestions(db) }
-        binding.textQuestions.setOnClickListener { showTextQuestions(db) }
-        binding.imageQuestions.setOnClickListener { showImageQuestions(db) }
+        binding.randomQuestions.setOnClickListener { showRandomQuestions() }
+        binding.textQuestions.setOnClickListener { showTextQuestions() }
+        binding.imageQuestions.setOnClickListener { showImageQuestions() }
         binding.yourResults.setOnClickListener { showResults() }
     }
 
-    private fun showRandomQuestions(db: AppDatabase) {
+    private fun showRandomQuestions() {
         val countOfTextQuestion = (2..countOfQuestions - 2).random()
         val countOfImageQuestion = countOfQuestions - countOfTextQuestion
 
@@ -48,17 +50,19 @@ class MainActivity : AppCompatActivity() {
         startQuizActivity(questions, bestScore)
     }
 
-    private fun showTextQuestions(db: AppDatabase) {
+    private fun showTextQuestions() {
+        Log.d("Tag", "here")
         val countOfRaw = db.textQuestionDao().getCount()
         val ids: MutableSet<Int> = mutableSetOf()
         while (ids.size < countOfQuestions) { ids.add((1..countOfRaw).random()) }
 
+        Log.d("Tag", "here")
         val questions = db.textQuestionDao().loadAllByIds(ids.toIntArray())
         val bestScore = db.scoreDao().findById(2)
         startQuizActivity(questions, bestScore)
     }
 
-    private fun showImageQuestions(db: AppDatabase) {
+    private fun showImageQuestions() {
         val countOfRaw = db.imageQuestionDao().getCount()
         val ids: MutableSet<Int> = mutableSetOf()
         while (ids.size < countOfQuestions) { ids.add((1..countOfRaw).random()) }
@@ -78,6 +82,8 @@ class MainActivity : AppCompatActivity() {
 
     private fun showResults() {
         val intent = Intent(this, ResultsActivity::class.java)
+        val scores = db.scoreDao().getAll().toTypedArray()
+        intent.putExtra("scores", scores)
         startActivity(intent)
     }
 }
--- app/src/main/java/ru/mytest/onlybtsfuns/ResultsActivity.kt
+++ app/src/main/java/ru/mytest/onlybtsfuns/ResultsActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.room.Room
 import ru.mytest.onlybtsfuns.data.AppDatabase
+import ru.mytest.onlybtsfuns.data.Score
 import ru.mytest.onlybtsfuns.databinding.ActivityResultsBinding
 
 class ResultsActivity : AppCompatActivity() {
@@ -15,13 +16,13 @@ class ResultsActivity : AppCompatActivity() {
         binding = ActivityResultsBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val db = Room.databaseBuilder(applicationContext, AppDatabase::class.java, "db.db")
-            .allowMainThreadQueries().build()
-        val scores = db.scoreDao().getAll()
+        val scores = intent.getParcelableArrayExtra("scores") as Array<*>
 
-        binding.noCategoryScore.text = scores[0].score.toString()
-        binding.textQuestionsScore.text = scores[1].score.toString()
-        binding.imageQuestionsScore.text = scores[2].score.toString()
+        if (scores[0] is Score) {
+            binding.noCategoryScore.text = (scores[0] as Score).score.toString()
+            binding.textQuestionsScore.text = (scores[1] as Score).score.toString()
+            binding.imageQuestionsScore.text = (scores[2] as Score).score.toString()
+        }
 
         binding.toCategories.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
